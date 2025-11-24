a8ec01e8c087e3ab843b898e27b65f148d37c53d
Junk3r3001 <spita559@gmail.com>
2025-11-24 00:17:52 +0000

    Add files via upload

---
+++ 9.Hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void hello(){              // function definition
+  cout << "Hello" << endl; // output to console
+}
+
+int main(){
+
+  hello();                 // call the function
+
+  return 0;
+}
