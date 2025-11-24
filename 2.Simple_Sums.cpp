7361a5b7726fa6bb556e923c9a12f7311e8e9c63
Junk3r3001 <spita559@gmail.com>
2025-11-24 00:16:04 +0000

    Add files via upload

---
+++ 2.Simple_Sums.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main() {
+    // Step 1: Declare an integer variable and initialize with 8
+    int num1 = 8;
+
+    // Step 2: Declare another integer variable and initialize with 10
+    int num2 = 10;
+
+    // Step 3: Declare a third integer variable that adds the previous two
+    int sum = num1 + num2;
+
+    // Step 4: Print the value of the sum to the console
+    std::cout << "The sum is: " << sum << std::endl;
+
+    return 0;
+}
