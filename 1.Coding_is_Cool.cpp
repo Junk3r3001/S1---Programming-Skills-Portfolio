b0c0a8946be6ce4cb778c90e78b93e587e569d47
Junk3r3001 <spita559@gmail.com>
2025-11-24 00:15:40 +0000

    Add files via upload

---
+++ 1.Coding_is_Cool.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    //cout - standard output stream in C++
+     cout << "Coding is Cool" << endl;
+    //<< endl - ends the line and flushes the output buffer.
+}
